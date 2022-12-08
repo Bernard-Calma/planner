09c878e6cc1ced3a5626561110152d8d345076ce
Bernard-Calma <nard.calma@gmail.com>
2022-12-07 22:26:09 -0800

    react app created and created landing page

---
+++ src/containers/LandingPage.jsx
@@ -0,0 +1,19 @@
+const LandingPage = () => {
+
+    return(
+        <div style = {styles.container}>
+            <p style = {styles.textTest }>landingPage</p>
+        </div>
+    )
+}
+
+export default LandingPage
+
+const styles = {
+    container: {
+        border: "1",
+    },
+    textTest: {
+        fontSize: "100px"
+    }
+}
