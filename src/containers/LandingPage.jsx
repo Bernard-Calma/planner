bfc437a9ff20080caa065290b513fe8c06ad8172
Bernard-Calma <nard.calma@gmail.com>
2022-12-09 20:27:36 -0800

    created an account model for temporary data and created a get balance function

--- src/containers/LandingPage.jsx
+++ src/containers/LandingPage.jsx
@@ -1,19 +1,47 @@
+import { useEffect, useState } from "react"
+
+import account from "../models/account"
+
 const LandingPage = () => {
+    const [balance, setBalance] = useState(0)
+
+    const styles = {
+        container: {
+            border: "1px solid red",
+        },
+        textTest: {
+            fontSize: "10vw"
+        },
+        income: {
+            backgroundColor: "green"
+        },
+        balance: {
+            backgroundColor: balance > 0 ? "green" : "red"
+        },
+        expenses: {
+            backgroundColor: "red"
+        }
+    }
+
+    const getBalance = () => {
+        setBalance(account.totalIncome - account.totalExpense)
+    }
+
+    useEffect(() => {
+        getBalance()
+    }, [])
 
     return(
         <div style = {styles.container}>
-            <p style = {styles.textTest }>landingPage</p>
+            <div>
+                <p style = {styles.income}>Income: ${account.totalIncome}</p>
+                <p style = {styles.balance}>Balance: ${balance}</p>
+                <p style = {styles.expenses}>Expenses: ${account.totalExpense}</p>
+            </div>
         </div>
     )
 }
 
 export default LandingPage
 
-const styles = {
-    container: {
-        border: "1px solid red",
-    },
-    textTest: {
-        fontSize: "10vw"
-    }
-}
+
---
+++ src/models/account.js
@@ -0,0 +1,7 @@
+// Sample balance
+const account = {
+    totalIncome: 100.00,
+    totalExpense: 50.00
+}
+
+module.exports = account
